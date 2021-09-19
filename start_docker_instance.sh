f6305bfeaafd69451d3b724d7eb2d0a63ed0111e
Sai Ramana Kiran <skiran@quantitativebrokers.com>
2021-09-19 11:06:17 +0530

    start change

--- start_docker_instance.sh
+++ start_docker_instance.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-docker run -it --name gcc11 -v${HOME}/workspace/test/:/root/c++test --rm somidi/gcctest:v1
+docker run -it --name gcc11 -v${HOME}/workspace/test/:/root/c++test --rm somidi/gcc11:v1
